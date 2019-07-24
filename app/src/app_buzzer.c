29f841de7592dacf922c0ee6f8a770078d598738
Ivo Clarysse <ivo@bcdevices.com>
2019-07-24 10:26:31 -0700

    melody

--- app/src/app_buzzer.c
+++ app/src/app_buzzer.c
@@ -18,7 +18,8 @@
 
 #define PERIOD_INIT    1500
 #define BUZZER_PIN     22
-#define BEEP_DURATION  K_MSEC(60)
+#define BEEP_DURATION  K_MSEC(10)
+#define PAUSE_DURATION  K_MSEC(1)
 #define US_TO_HZ(_us)  (USEC_PER_SEC / (_us))
 
 #if defined(CONFIG_SOC_FAMILY_NRF)
@@ -26,7 +27,7 @@
 #define PWM_DRIVER CONFIG_PWM_NRF5_SW_0_DEV_NAME
 #else
 #define PWM_DRIVER DT_NORDIC_NRF_PWM_PWM_0_LABEL
-#endif  /* CONFIG_PWM_NRF5_SW */
+#endif				/* CONFIG_PWM_NRF5_SW */
 #define PWM_CHANNEL BUZZER_PIN
 #elif defined(PWM_BUZZER0_PWM_CONTROLLER) && defined(PWM_BUZZER0_PWM_CHANNEL)
 /* get the defines from dt (based on alias 'pwm-buzzer0') */
@@ -38,18 +39,34 @@
 
 static u32_t period = PERIOD_INIT;
 
+static u32_t note_periods[] = {
+    1000000U / 988,		/* B */
+    1000000U / 880,		/* A */
+    1000000U / 784,		/* G */
+    1000000U / 699,		/* F */
+    1000000U / 659,		/* E */
+    1000000U / 587,		/* D */
+    1000000U / 523		/* C */
+};
+
+#define ARRAY_LEN(a) (sizeof(a)/sizeof((a)[0]))
+
 int app_buzzer_setup(void)
 {
     struct device *pwm;
 
     pwm = device_get_binding(PWM_DRIVER);
     if (NULL == pwm) {
-        return -1;
+	return -1;
     }
 
-    pwm_pin_set_usec(pwm, PWM_CHANNEL, period, period / 2U);
-    k_sleep(BEEP_DURATION);
-    pwm_pin_set_usec(pwm, PWM_CHANNEL, 0, 0);
+    for (int i = 0; i < ARRAY_LEN(note_periods); i++) {
+	period = note_periods[i];
+	pwm_pin_set_usec(pwm, PWM_CHANNEL, period, period / 2U);
+	k_sleep(BEEP_DURATION);
+	pwm_pin_set_usec(pwm, PWM_CHANNEL, period, 0);
+	k_sleep(PAUSE_DURATION);
+    }
 
     return 0;
 }
