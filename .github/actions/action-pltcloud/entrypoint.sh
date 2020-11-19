56f8a971b55641d26e6683e61c154b9f11fb827c
Eric Rauer <erauer@users.noreply.github.com>
2020-11-19 07:59:59 -0800

    Publish demo board firmware as GitHub Artifact and filter upload to PLTcloud (#35)

--- .github/actions/action-pltcloud/entrypoint.sh
+++ .github/actions/action-pltcloud/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-pltcloud -t "$API_TOKEN" -f "dist/*" -v "${GITHUB_REF:10}" -p "$PROJECT_UUID"
+pltcloud -t "$API_TOKEN" -f "dist/*demo*" -v "${GITHUB_REF:10}" -p "$PROJECT_UUID"
--- .github/actions/action-zephyr/entrypoint.sh
+++ .github/actions/action-zephyr/entrypoint.sh
@@ -1,3 +1,3 @@
-#!/bin/sh -l
+#!/bin/sh
 
-sh -c "make dist"
+make dist
